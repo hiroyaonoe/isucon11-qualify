d982666d737f99d2bdcf4dd332678800b40a7b9d
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-10-18 23:00:58 +0900

    fix run-bench

--- script/run-bench.sh
+++ script/run-bench.sh
@@ -5,4 +5,4 @@ BENCH_IP=$2
 
 cd ~/bench
 # ./bench -tls -target=localhost -all-addresses=localhost -jia-service-url http://localhost:5000
-./bench -tls -target=$WEBAPP_IP -all-addresses=$WEBAPP_IP -jia-service-url http://BENCH_IP:5000
+./bench -tls -target=$WEBAPP_IP -all-addresses=$WEBAPP_IP -jia-service-url http://$BENCH_IP:5000

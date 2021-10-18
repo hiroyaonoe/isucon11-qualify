75a2f7c9fbec3e3a538b10620784c6238f0075f2
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-10-19 00:39:47 +0900

    ConditionLevelをDBに保存する

--- go/main.go
+++ go/main.go
@@ -87,6 +87,7 @@ type IsuCondition struct {
 	Timestamp  time.Time `db:"timestamp"`
 	IsSitting  bool      `db:"is_sitting"`
 	Condition  string    `db:"condition"`
+	ConditionLevel string `db:"condition_level"`
 	Message    string    `db:"message"`
 	CreatedAt  time.Time `db:"created_at"`
 }
@@ -1208,19 +1209,24 @@ func postIsuCondition(c echo.Context) error {
 			return c.String(http.StatusBadRequest, "bad request body")
 		}
 
+		cLevel, err := calculateConditionLevel(cond.Condition)
+		if err != nil {
+			return c.String(http.StatusBadRequest, "bad request body")
+		}
 		rows = append(rows, IsuCondition{
 			JIAIsuUUID: jiaIsuUUID,
 			Timestamp: timestamp,
 			IsSitting: cond.IsSitting,
 			Condition: cond.Condition,
+			ConditionLevel: cLevel,
 			Message: cond.Message,
 		})
 	}
 
 	_, err = tx.Exec(
 		"INSERT INTO `isu_condition`"+
-			"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
-			"	VALUES (:jia_isu_uuid, :timestamp, :is_sitting, :condition, :message)",
+			"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `condition_level`, `message`)"+
+			"	VALUES (:jia_isu_uuid, :timestamp, :is_sitting, :condition, :condition_level, :message)",
 		rows)
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)

29ceeba2b49c7da1a1b7e325eb0c423e829cb0c8
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-10-22 17:22:45 +0000

    ベンチマークスクリプトにIPアドレスをベタ書き

--- script/run-bench.sh
+++ script/run-bench.sh
@@ -1,8 +1,10 @@
 #! /bin/bash -ex
 
-WEBAPP_IP=$1
-BENCH_IP=$2
+SERVER1_IP=172.31.33.136
+SERVER2_IP=172.31.45.3
+SERVER3_IP=
+BENCH_IP=172.31.32.80
 
 cd ~/bench
 # ./bench -tls -target=localhost -all-addresses=localhost -jia-service-url http://localhost:5000
-./bench -tls -target=$WEBAPP_IP -all-addresses=$WEBAPP_IP -jia-service-url http://$BENCH_IP:5000
+./bench -tls -target=$SERVER1_IP -all-addresses=$SERVER1_IP,$SERVER2_IP,$SERVER3_IP -jia-service-url http://$BENCH_IP:5000

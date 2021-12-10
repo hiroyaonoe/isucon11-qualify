347ea55cbea34cb598b621a1f621d67cad478589
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-12-10 22:20:41 +0900

    ひとまずのterraformの設定を追加

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type" {
+    description = "instance type"
+    type        = string
+    default     = "t2.micro"
+}
+
+variable "isucon11q_ami" {
+    description = "https://github.com/matsuu/aws-isucon"
+    type        = string
+    default     = "ami-01730fadc0a1d1e1c"
+}
+
+locals {
+    init_instance_script = file("${path.module}/init_instance.sh")
+}

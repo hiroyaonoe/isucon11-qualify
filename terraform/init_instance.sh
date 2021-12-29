c1b066d483c131f54ec487ffc58c20148c354547
Hiroya Onoe <hiroyanoe.io@gmail.com>
2021-12-30 08:27:12 +0900

    init_instanceのログが見れるように修正

--- terraform/init_instance.sh
+++ terraform/init_instance.sh
@@ -3,4 +3,7 @@
 LOG_OUT=/tmp/stdout.log
 LOG_ERR=/tmp/stderr.log
 
+exec 1> >(tee -a $LOG_OUT)
+exec 2>>$LOG_ERR
+
 su - isucon -c 'curl https://github.com/hiroyaonoe.keys >> /home/isucon/.ssh/authorized_keys'
